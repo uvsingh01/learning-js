fc565471098dd99c0b066b4d207ff2f4a1208e88
uvsingh01 <104893892+uvsingh01@users.noreply.github.com>
2022-08-02 01:38:20 +0530

    seventh

--- javaScript/fundamentals/array/basicarray.js
+++ javaScript/fundamentals/array/basicarray.js
@@ -10,4 +10,22 @@ console.log(newLength);
 friends.unshift("Aniket");
 console.log(friends);
 
-// 
+// remove element from last
+friends.pop();
+const popped =friends.pop();
+console.log(popped);
+console.log(friends);
+
+// remove element from first
+friends.shift();
+console.log(friends);
+
+//  to find the index of an element
+console.log(friends.indexOf('Raj'));
+console.log(friends.indexOf("Bob")); //this will print -1 because its not present in array
+console.log(friends.indexOf("popss"));//this will also print -1 because its not present in array
+
+// to check something is present in array or not
+
+console.log(friends.includes("Raj"));// this will give true because its present in array
+console.log(friends.includes("Puru")); // this will give false because its not present in array
