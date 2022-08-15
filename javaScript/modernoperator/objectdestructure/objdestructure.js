289ba515e6fc1a30e5fcda722ab7bf85fa98fa88
uvsingh01 <104893892+uvsingh01@users.noreply.github.com>
2022-08-15 23:31:29 +0530

    eighteenth

---
+++ javaScript/modernoperator/objectdestructure/objdestructure.js
@@ -0,0 +1,11 @@
+const obj={
+    fName:"Yuvraj",
+    lName:"Singh",
+    contactNo:"0123456789",
+    method:function(){
+        console.log("true");
+    }
+}
+
+const {a,b,c} = obj;
+console.log(a,b,c);
