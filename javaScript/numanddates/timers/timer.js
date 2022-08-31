cd3d8db9cde269ed403ca7ed3d2092a25a3d42e2
uvsingh01 <104893892+uvsingh01@users.noreply.github.com>
2022-08-31 23:42:54 +0530

    thirty third

--- javaScript/numanddates/timers/timer.js
+++ javaScript/numanddates/timers/timer.js
@@ -3,4 +3,21 @@
 // its an asynchronous function
 // it accepts callback functions
 
-setTimeout()
+const tim=setTimeout(function(){
+    console.log("hello");
+},5000);
+
+// setInterval
+// its a method to call something out after some time agin and again
+// it also accept callback functions
+
+const int = setInterval(function(){
+    console.log(new Date());
+},1000);
+
+// to stop these timers - use clearTimeout
+setTimeout(function(){
+    clearTimeout(int);
+},8000);
+
+// use case of timers
