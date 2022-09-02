f3ce41510b9729cc2dfa8fcb52a5dbd186c07083
uvsingh01 <104893892+uvsingh01@users.noreply.github.com>
2022-09-02 23:55:12 +0530

    thirty fourth

---
+++ javaScript/advancedom/working/domworking.js
@@ -0,0 +1,14 @@
+// when the browser reads the html file and when it  found html elements like 
+// body, head,html etc it creates a node for each and every element.
+// which results in a dom tree
+
+// each node have its type (node is the baap of all these four)
+// 1)element - button, div etc
+// 2)text - innertext   
+// 3)comment - 
+// 4)document - queryselector , getidbyelement
+
+// each node have inheritence from their parent that is node
+
+//node itself have parent that is eventtarget -which contains eventlisteners 
+
--- javaScript/numanddates/timers/timer.js
+++ javaScript/numanddates/timers/timer.js
@@ -5,7 +5,7 @@
 
 const tim=setTimeout(function(){
     console.log("hello");
-},5000);
+},000);
 
 // setInterval
 // its a method to call something out after some time agin and again
@@ -13,11 +13,24 @@ const tim=setTimeout(function(){
 
 const int = setInterval(function(){
     console.log(new Date());
-},1000);
+},0);
 
 // to stop these timers - use clearTimeout
 setTimeout(function(){
     clearTimeout(int);
-},8000);
+},0);
 
-// use case of timers
+// countdown
+
+let time=10;
+let timer = setInterval(function(){
+    let min =String(Math.trunc(time/60)).padStart(2,0);//we convert these into string to format them like 01:00;
+    let sec=String(time%60).padStart(2,0);//we used trunc to remove the decimals from the mintues
+    console.log(`${min}:${sec}`);
+    
+    if(time===0){
+        clearInterval(timer);
+    }
+    time--;
+},1000)
+
