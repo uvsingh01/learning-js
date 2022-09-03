f8fe6c9f6029d034a50f0f21b2972084a8e519d1
uvsingh01 <104893892+uvsingh01@users.noreply.github.com>
2022-09-03 23:35:54 +0530

    thirty fifth

--- javaScript/advancedom/working/domworking.js
+++ javaScript/advancedom/working/domworking.js
@@ -12,3 +12,47 @@
 
 //node itself have parent that is eventtarget -which contains eventlisteners 
 
+// 🔻 to select 
+
+document.getElementById("button")//to select something with id
+document.querySelector(".button");//to select something with class or id 
+document.querySelectorAll(".button")////to select all with id or class
+document.getElementsByClassName("button");////to select something with class 
+document.getElementsByTagName("button");//to select something with tag name 
+
+// note :- the tagname and classname will update it nodes whenever their is change or deletion in elements
+
+
+// 🔻to create
+
+let b = document.querySelector(".bod");
+let ele = document.createElement("div"); //the element is now created
+// now add something in that element
+ele.textContent = "hey connections";
+// if we want to add html inside this ele then we can use innerhtml
+// ele.innerHTML = `<div>haha</div>`
+// now we can add class attribute in this element
+ele.classList.add("message");
+// now we have to put it inside our dom then
+//  if we want to put it as first child then use prepend
+b.prepend(ele);
+// if we want to put it as last child then use append
+b.append(ele);
+// note:- if we try to add one element twice by ap   pend or prepend then the last method to 
+// add will be displayed because one element can be present only one time in dom
+// its like one person can go somewhere one place at a time
+
+// if we want to add same element multiple time then we can clone it 
+b.prepend(ele.cloneNode(true)) //if we pass true in clonenode then it will copy all the child elements inside it
+
+// 🔻to delete
+// document.querySelector(".message").addEventListener("click",function(e){
+//     ele.remove(); 
+//     console.log(e);
+// })
+
+document.querySelectorAll('.message').forEach(item => {
+  item.addEventListener('click', event => {
+    ele.remove();
+  })
+})
